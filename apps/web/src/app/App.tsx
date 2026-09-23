9e843ffed9666e17be5248514d7c35a0476bead3
Daniel Luiz Alves <daniel.xcoders@gmail.com>
2026-09-23 12:50:38 -0300

    chore: scaffold Palmr v4 web workspace

---
+++ apps/web/src/app/App.tsx
@@ -0,0 +1,9 @@
+import { ConfigProvider, Typography } from "antd";
+
+export function App() {
+  return (
+    <ConfigProvider>
+      <Typography.Title>Palmr</Typography.Title>
+    </ConfigProvider>
+  );
+}
